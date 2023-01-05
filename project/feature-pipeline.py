b40c9a844eaa74099390b4390fb25cad97759fce
William_S <wsta@mechakhan.com>
2023-01-05 18:47:58 +0100

    finalize feature and training pipeline, must still test feature pipeline without backfill

--- project/feature-pipeline.py
+++ project/feature-pipeline.py
@@ -1,38 +1,110 @@
 import requests
 import csv
+import pandas
+import hopsworks
+import modal
 
-starttime = "2022-11-06"
-endtime = "2022-12-28"
+from datetime import datetime
+from datetime import timedelta
 
-url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime="+starttime+"&endtime="+endtime
+# If false, run this script on Modal. Otherwise, run locally.
+LOCAL=True
 
-headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-}
+# If true, backfill data. Otherwise, load new data from the API and upload it to Hopsworks
+BACKFILL=True
 
-response = requests.request("GET", url, headers=headers, data={})
-myjson = response.json()
-ourdata = []
+# If BACKFILL is True, backfill data either from the API to a local CSV file or from a local CSV file to Hopsworks
+TO_CSV=False
 
-# print(response.status_code)
-samples_count = len(myjson["features"])
-print("Collected: " + str(samples_count) + " samples")
+BACKFILL_CSV_FILE = \
+"https://raw.githubusercontent.com/william-stacken/ID2223-labs/project/project/backfill.csv"
 
-for x in range(len(myjson["features"])):
-    properties = list(myjson["features"][x]["properties"].values())
-    coordinates = list(myjson["features"][x]["geometry"].values())[1]
+FEATURE_GROUP_NAME = "earthquake_pred"
+FEATURE_GROUP_DESCRIPTION="Earthquake prediction dataset"
+FEATURE_GROUP_VERSION=1
 
-    mag = properties[0]
-    time = properties[2]
+def api_request_df():
+	columns = ["latitude", "longitude", "depth", "mag", "time"]
 
-    latitude = coordinates[0]
-    longitude = coordinates[1]
-    depth = coordinates[2]
+	if BACKFILL:
+		starttime = "2022-11-06"
+		endtime = "2022-12-28"
+	else:
+		now = datetime.now()
+		starttime = (now - timedelta(days=1)).strftime("%Y-%m-%d")
+		endtime = now.strftime("%Y-%m-%d")
 
-    ourdata.append([latitude, longitude, depth, mag, time])
+	url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime="+starttime+"&endtime="+endtime
 
-with open("database.csv", "w", encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(["latitude", "longitude", "depth", "mag", "time"])
-    writer.writerows(ourdata)
+	headers = {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	}
+
+	response = requests.request("GET", url, headers=headers, data={})
+	myjson = response.json()
+	ourdata = []
+
+	# print(response.status_code)
+	samples_count = len(myjson["features"])
+	print("Collected: " + str(samples_count) + " samples")
+
+	for x in range(len(myjson["features"])):
+		properties = list(myjson["features"][x]["properties"].values())
+		coordinates = list(myjson["features"][x]["geometry"].values())[1]
+
+		mag = properties[0]
+		time = properties[2]
+
+		latitude = coordinates[0]
+		longitude = coordinates[1]
+		depth = coordinates[2]
+
+		ourdata.append([latitude, longitude, depth, mag, time])
+
+	df = pandas.DataFrame.from_records(ourdata, columns=columns)
+
+	return df
+
+def dataframe_cleaning(df):
+	# Drop incomplete entries and normalize the timestamp
+	# to prevent gradient explosion
+	df = df.dropna()
+	df["time"] = df["time"] / 1E10
+
+	return df
+
+def main():
+	if BACKFILL and not TO_CSV:
+		df = pandas.read_csv(BACKFILL_CSV_FILE)
+	else:
+		df = api_request_df()
+
+	if BACKFILL and TO_CSV:
+		df.to_csv(csv_file)
+	else:
+		df = dataframe_cleaning(df)
+
+		hw = hopsworks.login()
+		feature_store = hw.get_feature_store()
+		feature_group = feature_store.get_or_create_feature_group(
+			name=FEATURE_GROUP_NAME,
+			description=FEATURE_GROUP_DESCRIPTION,
+			version=FEATURE_GROUP_VERSION,
+			primary_key=["latitude", "longitude", "time"]
+		)
+		feature_group.insert(df, write_options={"wait_for_job": False})
+
+if __name__ == "__main__":
+	if LOCAL:
+		main()
+	else:
+		stub = modal.Stub()
+		image = modal.Image.debian_slim().apt_install(["libgomp1"]).pip_install(["hopsworks==3.0.4"])
+
+		@stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+		def modal_main():
+			main()
+
+		with stub.run():
+			modal_main()
---
+++ project/training-pipeline.py
@@ -0,0 +1,117 @@
+import os
+import hopsworks
+import numpy
+import seaborn
+import pandas
+from hsml.schema import Schema
+from hsml.model_schema import ModelSchema
+
+from tensorflow import keras
+from keras.callbacks import EarlyStopping
+
+# If false, run this script on Modal. Otherwise, run locally.
+LOCAL=True
+
+FEATURE_GROUP_NAME = "earthquake_pred"
+FEATURE_GROUP_VERSION=1
+
+FEATURE_VIEW_NAME = "earthquake_pred_view"
+FEATURE_VIEW_DESCRIPTION="Earthquake prediction dataset"
+FEATURE_VIEW_VERSION=1
+
+MODEL_NAME = "earthquake_model"
+MODEL_DESCRIPTION = "Earthquake magnitude and depth predictor"
+MODEL_DIRECTORY = MODEL_NAME
+
+loss = 'mse'
+optimizer = 'adam'
+metrics = ['accuracy']
+
+def plot_history(model_fit_log):
+	met = numpy.asarray(model_fit_log.history[metrics[0]])
+	val_met = numpy.asarray(model_fit_log.history['val_' + metrics[0]])
+
+	values = numpy.zeros((len(met), 2), dtype=float)
+	values[:,0] = met
+	values[:,1] = val_met
+
+	seaborn.set(style="whitegrid")
+	return seaborn.lineplot(data=pandas.DataFrame(
+		values,
+		pandas.RangeIndex(start=0, stop=len(met)),
+		columns=[
+			"training-" + metrics[0],
+			"validation-" + metrics[0]
+		]
+	))
+
+def main():
+	hw = hopsworks.login()
+	feature_store = hw.get_feature_store()
+
+	try:
+		feature_view = feature_store.get_feature_view(name=FEATURE_VIEW_NAME, version=FEATURE_VIEW_VERSION)
+	except:
+		feature_view = feature_store.create_feature_view(
+			name=FEATURE_VIEW_NAME,
+			description=FEATURE_VIEW_DESCRIPTION,
+			version=FEATURE_VIEW_VERSION,
+			labels=["depth", "mag"],
+			query=feature_store.get_feature_group(
+				name=FEATURE_GROUP_NAME,
+				version=FEATURE_GROUP_VERSION
+			).select_all()
+		)
+
+	X_train, X_test, y_train, y_test = feature_view.train_test_split(0.15)
+
+	model = keras.models.Sequential([
+		keras.layers.Dense(32, activation="elu", kernel_initializer="he_normal", input_shape=(X_train.shape[1],)),
+		keras.layers.Dropout(rate=0.2),
+		keras.layers.Dense(32, activation="elu", kernel_initializer="he_normal"),
+		keras.layers.Dropout(rate=0.2),
+		keras.layers.Dense(32, activation="elu", kernel_initializer="he_normal"),
+		keras.layers.BatchNormalization(),
+		keras.layers.Dropout(rate=0.2),
+		keras.layers.Dense(y_train.shape[1], activation='softmax')
+	])
+
+	train_cb = EarlyStopping(monitor=metrics[0], min_delta=0.01, patience=3)
+
+	model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+	log = model.fit(X_train, y_train, batch_size=50, validation_split=0.15, epochs=30, callbacks=train_cb)
+	report = model.evaluate(X_test, y_test)
+
+	#plot_history(log)
+
+	if not os.path.isdir(MODEL_DIRECTORY):
+		os.mkdir(MODEL_DIRECTORY)
+	model.save(os.path.join(MODEL_DIRECTORY, MODEL_NAME + ".h5"))
+
+	schema_X = Schema(X_train)
+	schema_y = Schema(y_train)
+	model_schema = ModelSchema(schema_X, schema_y)
+
+	registry = hw.get_model_registry()
+	hw_model = registry.python.create_model(
+		name=MODEL_NAME,
+		description=MODEL_DESCRIPTION,
+		metrics={metrics[0]: report[1]},
+		model_schema=model_schema
+	)
+
+	hw_model.save(MODEL_DIRECTORY)
+
+if __name__ == "__main__":
+	if LOCAL:
+		main()
+	else:
+		stub = modal.Stub()
+		image = modal.Image.debian_slim().apt_install(["libgomp1"]).pip_install(["hopsworks==3.0.4", "seaborn", "tensorflow", "joblib"])
+
+		@stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+		def modal_main():
+			main()
+
+		with stub.run():
+			modal_main()
